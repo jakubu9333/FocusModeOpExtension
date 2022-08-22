268203acc9bd518377c82f8fd409bee568366c41
Jakub Uhlárik <xuhlarik@fi.muni.cz>
2022-08-22 23:16:05 +0200

    red-green button

--- background.js
+++ background.js
@@ -2,5 +2,6 @@ let def_name="Jakub"
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ def_name });
+    chrome.storage.local.set({def:0});
     console.log('Default name set to %cJakub', `name: ${def_name}`);
   });
